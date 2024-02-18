10fdcb909205417d08298679214c98f870aa3bcc
Baha <bahaahmedov@Bahas-MacBook-Air.local>
2024-02-18 13:38:25 +0500

    initial commit, real estate

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //HISTORY
+        final String title = "UYJOY";
+        final int founded_in = 2024;
+        final  String month = "February";
+        final String first_branch_country = "Uzbekistan";
+        final String first_branch_city = "Samarkand";
+
+        //DEPARTMENTS
+        String Dep1 = "It Department";
+        String Dep2 = "Smm Department";
+        String Dep3 = "Cyber Security Department";
+        String Dep4 = "Designers Department";
+        String Dep5 =  "Finance and Mortgage Department";
+        String Dep6 = "Customer Service";
+
+        //Amount of Deparments workers
+        byte It_Department = 25;
+        byte Smm_Department = 30;
+        byte Cyber_Security_Department = 12;
+        byte Designers_Department = 35;
+        byte Finance_and_Mortgage_Department = 23;
+        byte Customer_Service = 10;
+        short Total_Amount = 90;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
