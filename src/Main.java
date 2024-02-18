d85cc4ed24945317a691f6594477872cc940e51e
Baha <bahaahmedov@Bahas-MacBook-Air.local>
2024-02-18 16:03:20 +0500

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+
         //HISTORY
         final String title = "UYJOY";
         final int founded_in = 2024;
         final  String month = "February";
         final String first_branch_country = "Uzbekistan";
         final String first_branch_city = "Samarkand";
+
 
         //DEPARTMENTS
         String Dep1 = "It Department";
@@ -16,24 +18,62 @@ public class Main {
         String Dep5 =  "Finance and Mortgage Department";
         String Dep6 = "Customer Service";
 
-        //Amount of Deparments workers
+
+        //Amount of Departments workers
         byte It_Department = 25;
         byte Smm_Department = 30;
         byte Cyber_Security_Department = 12;
         byte Designers_Department = 35;
         byte Finance_and_Mortgage_Department = 23;
         byte Customer_Service = 10;
-        short Total_Amount = 90;
+        short Total_Amount = 135;
 
 
+        //SALARY
+        int itSalary = 5000;
+        int smmSalary = 4500;
+        int cyberSecuritySalary = 5500;
+        int designersSalary = 6000;
+        int financeAndMortgageSalary = 4800;
+        int customerServiceSalary = 4000;
+        int Others = 1000; // Cleaning, Delivery, Media, Departments
 
 
+        //TAXES
+        double Total_Taxes_Workers = 125697.5;
+        double Tax_for_Property = 3802.24;
+        float Google_Play_Store = 30.0f; // in percent (30%)
+        float Apple_App_Store = 30.0f;// in percent (30%)
+        float Government_Tax = 25.0f;// in percent (25%)
 
 
+        //PARTNERS
+        String Org1 = "Furniture Factory";
+        String Org2 = "Delivery Company";
+        String Org3 = "Construction Company";
 
 
+        //Branches
+        byte Uzbekistan = 10;
+        byte America = 2;
+        byte Russia = 4;
+        byte China = 1;
 
 
+        //Branches Status(Working or Not)
+        boolean American_1st_Branch = false;//in Progress(Soon will be opened)
+        boolean Russian_2nd_Branch = false;//in Progress(Soon will be opened)
+        boolean Uzbekistan_9th_Branch = false;//in Progress(Soon will be opened)
+        boolean Uzbekistan_10th_Branch = false;//in Progress(Soon will be opened)
+
+
+        //Productivity of Department Grading System(A:5, B:4, C:3, D:2, E:1)
+        char It_Department_Grade = 'A';
+        char Smm_Department_Grade = 'A';
+        char Cyber_Security_Department_Grade = 'A';
+        char Designers_Department_Grade = 'C';
+        char Finance_and_Mortgage_Department_Grade = 'A';
+        char Customer_Service_Grade = 'B';
 
     }
 }
